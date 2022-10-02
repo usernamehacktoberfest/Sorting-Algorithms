162e76f726c6e97871bababdcd90506ff0090c7d
PriyanshuGupta18 <65297674+PriyanshuGupta18@users.noreply.github.com>
2022-10-02 17:57:12 +0530

    Create bubble_sort.py

---
+++ python/bubble_sort.py
@@ -0,0 +1,15 @@
+def bubblesort(elements):
+	swapped = False
+	for n in range(len(elements)-1, 0, -1):
+		for i in range(n):
+			if elements[i] > elements[i + 1]:
+				swapped = True
+				elements[i], elements[i + 1] = elements[i + 1], elements[i]	
+		if not swapped:
+			return
+elements = [39, 12, 18, 85, 72, 10, 2, 18]
+print("Unsorted list is,")
+print(elements)
+bubblesort(elements)
+print("Sorted Array is, ")
+print(elements)
